41d63725a3f3111fb36474a4f94b088cd25c045d
dheeraj-0-0-7 <60390103+dheeraj-0-0-7@users.noreply.github.com>
2023-10-30 10:48:44 -0700

    Update Duplicate_example.py

--- Duplicate_example.py
+++ Duplicate_example.py
@@ -8,7 +8,9 @@ def calculate_volume(length, width, height):
     return calculate_area(length, width) * height
 
 def calculate_surface_area(length, width, height):
-    return 2 * (calculate_area(length, width) + length * height + width * height)
+    base_area = calculate_area(length, width)
+    lateral_area = 2 * (length * height + width * height)
+    return 2 * base_area + lateral_area
 
 def calculate_area_and_perimeter(length, width):
     area = calculate_area(length, width)
