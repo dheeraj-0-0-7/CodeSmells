6085eae9305538c361b2bf34d4bd734fd0706cac
dheeraj-0-0-7 <60390103+dheeraj-0-0-7@users.noreply.github.com>
2023-10-30 10:26:44 -0700

    Update Code_smells_example.py

--- Code_smells_example.py
+++ Code_smells_example.py
@@ -1,10 +1,14 @@
 #Code Smells Example for long method
+#Refactored code
+def apply_discount(price):
+    if price > 100:
+        return price * 0.9
+    else:
+        return price * 0.95
+
 def calculate_total_price(items):
     total = 0
     for item in items:
         if item.quantity > 0:
-            if item.price > 100:
-                total += item.price * 0.9
-            else:
-                total += item.price * 0.95
+            total += apply_discount(item.price)
     return total
